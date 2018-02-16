114520c85bd5ad0b19ddcf23fe85a96a4299645e
Timoffei Bubigo <timoffei.bubigo@outlook.com>
2018-02-16 16:51:47 +0300

    Add update message method

--- src/RocketChatMessage.php
+++ src/RocketChatMessage.php
@@ -31,5 +31,19 @@ class Message extends Client {
         }
     }
 
+	public function update( $text, $roomId ) {
+		$response = Request::post( $this->api . 'chat.update' )
+            ->body( array('roomId' => $roomId, 'msgId' => $this->id, 'text' => $text) )
+            ->send();
+			
+        if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+            return true;
+        } else {
+            if( isset($response->body->error) )	echo( $response->body->error . "\n" );
+            else if( isset($response->body->message) )	echo( $response->body->message . "\n" );
+            return false;
+        }
+	}
+	
 }
 
