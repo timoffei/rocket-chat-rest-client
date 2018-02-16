becc23b8c29ac4c0d441b9702b02e5873c04de54
Timoffei Bubigo <timoffei.bubigo@outlook.com>
2018-02-16 12:32:57 +0300

    Add Chat Direct

---
+++ src/RocketChatChat.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace RocketChat;
+
+use Httpful\Request;
+use RocketChat\Client;
+
+class Chat extends Client {
+
+    public $id;
+    public $name;
+    public $members = array();
+
+    public function __construct($name, $members = array()){
+        parent::__construct();
+        if( is_string($name) ) {
+            $this->name = $name;
+        } else if( isset($name->_id) ) {
+            $this->name = $name->name;
+            $this->id = $name->_id;
+        }
+        foreach($members as $member){
+            if( is_a($member, '\RocketChat\User') ) {
+                $this->members[] = $member;
+            } else if( is_string($member) ) {
+                // TODO
+                $this->members[] = new User($member);
+            }
+        }
+    }
+
+    /**
+     * Post a message in this channel, as the logged-in user
+     */
+    public function postMessage( $text ) {
+        $message = is_string($text) ? array( 'text' => $text ) : $text;
+        if( !isset($message['attachments']) ){
+            $message['attachments'] = array();
+        }
+
+        $response = Request::post( $this->api . 'chat.postMessage' )
+            ->body( array_merge(array('channel' => '@'.$this->name), $message) )
+            ->send();
+
+        if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+            return $response->body;
+        } else {
+            if( isset($response->body->error) )	echo( $response->body->error . "\n" );
+            else if( isset($response->body->message) )	echo( $response->body->message . "\n" );
+            return false;
+        }
+    }
+
+}
+
